8d6ef284f5af6d120dd4df045cdede9149a622e0
slaweykow <slaweykow@protonmail.com>
2019-12-11 23:41:02 +0200

    More styling, Home page, About page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class AboutComponent extends Component {
     constructor(props) {
@@ -8,7 +9,12 @@ class AboutComponent extends Component {
     render() {
         return (
             <div className="container">
-                <h1>About</h1>
+                <h1>Code sharing made <em>simple</em>.</h1>
+
+                <p>DevBin is a web application for sharing code between developers. It aims to ease the work of CompSci students and developers.</p>
+                <p>Built using the MERN stack, serving as a final project (exam) for the React.JS course at the Software University.</p>
+                
+                <a target="_blank" href="https://github.com/slaweykow/DevBin" className="about-link">Github Repository</a> | Licensed under the <a target="_blank" href="https://github.com/slaweykow/DevBin/blob/master/LICENSE.md" className="about-link">MIT</a> license 
             </div>
         );
     }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
+
+import './Home.css';
 
 class HomeComponent extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            captcha: '',
+            paste: ''
+        }
+    }
+
+    handleCaptcha = val => {
+        if (val === null) val = 'expired';
+        this.setState({ captcha: val });
+    }
+
+    handleChange = input => {
+        this.setState({ paste: input });
+    }
+
+    onSubmit = () => {
+        if (this.state.captcha === 'expired' ||
+        this.state.captcha === '' ||
+        this.state.captcha === null) {
+            return console.log('Please verify you are not a robot.');
+        }
     }
 
     render() {
         return (
             <div className="container">
                 <h2>Note: You are not logged in. You will not be able to edit or delete anything you paste.</h2>
+
+                <textarea 
+                placeholder="// Happy pasting :)" 
+                onChange={e => this.handleChange(e.target.value)}
+                />
+
+                <ReCAPTCHA theme="dark" sitekey="6LevRccUAAAAAKBJ4D4F_ysTmvEz2btdnKBt3nda" onChange={val => this.handleCaptcha(val)} />
+
+                <button
+                className="btn-submit"
+                onClick={() => this.onSubmit()}>
+                    Submit
+                </button>
             </div>
         );
     }
