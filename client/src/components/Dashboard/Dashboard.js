6634cd25ddfbd56e1828c45521a0d6e2ae13dd88
slaweykow <slaweykow@protonmail.com>
2019-12-13 17:20:59 +0200

    Dashboard styling

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class DashboardComponent extends Component {
     constructor(props) {
@@ -7,8 +8,21 @@ class DashboardComponent extends Component {
 
     render() {
         return (
-            <div className="container">
-                <h1>Dashboard</h1>
+            <div className="container db">
+                <h1>Welcome [name]!</h1>
+                <p>You have pasted <Link to="/pastes/:id" className="db-link">[pastesAmount]</Link> pieces of code.</p>
+                <p>You have been a proud member of DevBin for <em>[days/months/years]</em>.</p>
+
+                <h2>Click <Link to="/pastes/:id" className="db-link">here</Link> if you wish to edit your profile.</h2>
+
+                <hr />
+
+                <h1>Administrator zone</h1>
+
+                <p>DevBin is currently used by <em>[number]</em> registered developers and is the home to <em>[number]</em> pieces of code.</p>
+
+                <Link to="/pastes/:id" className="db-link">User management</Link> | <Link to="/pastes/:id" className="db-link">Admin management</Link>
+
             </div>
         );
     }
