50a50c6b2ea5cdb03c266a1610c9cd471f2fac0a
slaweykow <slaweykow@protonmail.com>
2019-12-15 02:45:38 +0200

    server: Paste controller improvements

--- server/controllers/pasteController.js
+++ server/controllers/pasteController.js
@@ -1,23 +1,51 @@
 const { Paste, User } = require('../models');
 
 module.exports = {
-    get: {},
+    get: async (req, res, next) => {
+        const pasteId = req.params.id;
+
+        const found = await Paste.findById(pasteId);
+
+        if (!found) { res.status(404).send('Not found!'); return; }
+
+        res.status(200).send(found);
+    },
     post: async (req, res, next) => {
-        const { content } = req.body;
-        
-        const author = 'anonymous';
-        if (req.user)
-            author = req.user._id;
+        const { author, content } = req.body;
 
         try {
             const createdPaste = await Paste.create({ author, content });
-            if (author !== 'anonymous') await User.updateOne({ _id: author }, { $push: { pastes: createdPaste } });
+            if (author !== 'anonymous') await User.updateOne({ username: author }, { $push: { pastes: createdPaste } });
 
             res.status(200).send(createdPaste);
         } catch (e) {
             next(e);
         }
     },
-    put: {},
-    delete: {}
+    put: (req, res, next) => {
+        const pasteId = req.params.id;
+        const { content } = req.body;
+        let newContent = '';
+
+        if (!content)
+            newContent = ''
+        else
+            newContent = content;
+
+        Paste.findOneAndUpdate({ _id: pasteId }, { content: newContent })
+            .then(() => res.status(200).send('Successfully edited!'))
+            .catch(next);
+    },
+    delete: async (req, res, next) => {
+        const pasteId = req.params.id;
+
+        try {
+            const removedPost = await Paste.findOneAndDelete({ _id: pasteId });
+            await Paste.updateOne({ username: removedPost.author }, { $pull: { posts: removedPost._id } });
+        
+            res.status(200).send('Successfully deleted!');
+        } catch(e) {
+            next(e);
+        }
+    }
 }
--- server/routes/paste.js
+++ server/routes/paste.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const { pasteController } = require('../controllers');
 
+router.get('/:id', pasteController.get);
+
 router.post('/create', pasteController.post);
+
+router.put('/:id', pasteController.put);
+
+router.delete('/:id', pasteController.delete);
 
 module.exports = router;
